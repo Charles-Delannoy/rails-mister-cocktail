fb1d57439cba82f2c265cd8756a118d201952b26
Charles Delannoy <charles.delannoy@hei.fr>
2020-08-16 20:48:31 +0200

    simili responsive

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
